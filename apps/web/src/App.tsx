a4af35680e154b60ac13d9da172da574063161ab
mjsamax <mjsamax@gmail.com>
2026-05-27 14:20:49 -0500

    establecer rutas de pages

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, ChatRoom, NotFound } from './pages';
+import './assets/global.css';
+
 export function App() {
   return (
-    <main>
-      <h1>Warachikuy</h1>
-      <p>Simulador de Entrevistas Laborales Adaptativo</p>
-    </main>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<ChatRoom />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
