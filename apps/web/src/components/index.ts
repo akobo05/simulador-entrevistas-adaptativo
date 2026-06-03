6df77c711ffece7962f7e7d17e6d34ea694551ba
mjsamax <mjsamax@gmail.com>
2026-06-03 00:22:04 -0500

    chore(components): export barrel index

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,11 @@
+export { Button } from './Button';
+export type { ButtonProps, ButtonVariant, ButtonSize } from './Button';
+
+export { Badge } from './Badge';
+export type { BadgeProps, BadgeColor } from './Badge';
+
+export { Card } from './Card';
+export type { CardProps } from './Card';
+
+export { ProgressRing } from './ProgressRing';
+export type { ProgressRingProps } from './ProgressRing';
